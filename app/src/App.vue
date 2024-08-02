c75690e918f6f5caacaa76d2bea6fcfab3a9ae9c
Pothuraju <ram.k160796@gmail.com>
2024-08-02 20:41:00 +0000

    app boilerplate

---
+++ app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <p>NuxtVue Hub </p><img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  top: 50%;
+  left: 50%;
+}
+
+.container p {
+  font-size: 4em;
+}
+</style>
