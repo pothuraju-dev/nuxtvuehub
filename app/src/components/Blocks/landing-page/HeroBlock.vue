e3df2712ab3ec5ea0685b990df6bda05087fdfed
Pothuraju <ram.k160796@gmail.com>
2024-08-04 04:28:52 +0000

    github path changed

--- app/src/components/Blocks/landing-page/HeroBlock.vue
+++ app/src/components/Blocks/landing-page/HeroBlock.vue
@@ -15,7 +15,7 @@
                         <iconify-icon icon="lucide:square-arrow-out-up-right"></iconify-icon>
                     </RouterLink>
                     <a class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-full text-base gap-x-2.5 px-3.5 py-2.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
-                        href="https://github.com/pothuraju-dev/nuxtvuehub" rel="noopener noreferrer" target="_blank">
+                        href="https://github.com/pothuraju-dev/nuxtvuehub/tree/main" rel="noopener noreferrer" target="_blank">
                         <iconify-icon icon="lucide:github"></iconify-icon>
                         <span class="">Use this template</span>
                     </a>
